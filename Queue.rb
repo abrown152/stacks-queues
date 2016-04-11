46c0f815c163084b27b68e4c42b3ca7defbbe756
Alysia Brown <alysiacbrown@gmail.com>
2016-04-10 20:11:32 -0700

    Done

--- Queue.rb
+++ Queue.rb
@@ -1,13 +1,17 @@
 class Queue
+  attr_accessor :waiting_workers
   def initialize
+    @waiting_workers = [1..10]
   end
-  
-  def dequeue
+
+  def dequeue(workers)
+    hired = @waiting_workers.pop(workers)
+    Stack.new(hired)
   end
-  
+
   def enqueue(element)
   end
-  
+
   def size
   end
 end
--- Stack.rb
+++ Stack.rb
@@ -1,14 +1,28 @@
 class Stack
-  def initialize
-    @store = Array.new
+  attr_accessor :current_workers
+  def initialize(current_workers)
+    @current_workers = current_workers
   end
-  
-  def pop
-  end
-  
+
   def push(element)
+    @current_workers << element
   end
-  
+
+  def pop
+    @current_workers.pop.rand(1..6)
+  end
+
+  def dequeue
+    @current_workers.shift
+  end
+
+  def enqueue(element)
+    @current_workers.insert(-1, element)
+  end
+
   def size
+  end
+
+  def empty?
   end
 end
